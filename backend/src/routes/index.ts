74794c6be4b20cbc888a3b245d52a4848dfd9fea
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2023-09-25 22:47:55 +0700

    [add] test controller

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,8 @@
 import express, { Router } from "express";
+import testController from "../controller/testController/test";
 
 const router: Router = express.Router();
 
-// router.use("/", testep);
+router.use("/", testController);
 
 export = router;
