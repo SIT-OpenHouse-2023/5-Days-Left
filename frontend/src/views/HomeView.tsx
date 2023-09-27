da3d43241488891962753ed34ddf5ad48962bc67
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2023-09-27 17:45:59 +0700

    [Edit] add color.ts for project colors

--- frontend/src/views/HomeView.tsx
+++ frontend/src/views/HomeView.tsx
@@ -1,5 +1,11 @@
+import React from "react";
+import { textHeader } from "../types/colors";
+
 function HomeView() {
-  return <>Hi</>;
+  const divStyle = {
+    color: textHeader,
+  };
+  return <div style={divStyle}>Hi</div>;
 }
 
 export default HomeView;
