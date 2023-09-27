15e8f2c25fde50cc2c09a84ba8d8e94a5ef99df8
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2023-09-27 15:00:27 +0700

    [Del] Unnescessary files

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import HomeView from "./views/HomeView";
-
-test("renders learn react link", () => {
-  render(<HomeView />);
-  const element = screen.getByText("Hi");
-  expect(element).toBeInTheDocument();
-});
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginView from "./views/LoginView";
 import HomeView from "./views/HomeView";
@@ -9,39 +8,34 @@ import ActivityView from "./views/ActivityView";
 import RegisterView from "./views/RegisterView";
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomeView />,
-    },
-    {
-        path: "/login",
-        element: <LoginView />,
-    },
-    {
-        path: "*",
-        element: <div>404</div>,
-    },
-    {
-        path: "/activity",
-        element: <ActivityView />,
-    },
-    {
-        path: "/register",
-        element: <RegisterView />,
-    },
+  {
+    path: "/",
+    element: <HomeView />,
+  },
+  {
+    path: "/login",
+    element: <LoginView />,
+  },
+  {
+    path: "*",
+    element: <div>404</div>,
+  },
+  {
+    path: "/activity",
+    element: <ActivityView />,
+  },
+  {
+    path: "/register",
+    element: <RegisterView />,
+  },
 ]);
 
 root.render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
